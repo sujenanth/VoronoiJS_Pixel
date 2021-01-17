ef2db52305b329e1080ba448abd660e8c952de1f
sujenanth <sujenan5@gmail.com>
2021-01-18 00:33:16 +0100

    frontend implented

--- voronoifrontend/src/App.js
+++ voronoifrontend/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
-import './App.css';
+import './stylesheet.css';
+import {Fab} from '@material-ui/core';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import Description from "./components/Description";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className={"block"}>
+            <div className={"bigTitle_block"}>
+                <h1>Voronoi</h1>
+                <span>Ein Gruppenprojekt von zwei IMS-Schülern</span>
+            </div>
+            <div className={"inline_block"}>
+                <h1>Voronoi</h1>
+                <p>
+                    Im Rahmen des Unterrichts der angewandten Mathematik hatten wir die Möglichkeit das mathematische Vornoi-Diagramm, auch Thiessen-Polygone oder Dirichlet-Zerlegung genannt, in einer Applikation umzuwandeln.
+                </p>
+                <p>
+                    Das Projekt wurde von zwei IMS-Schüler, <b>Sujenan Thirumamany und Ismail Solombrino</b> realisiert
+                </p>
+            </div>
+        </div>
+        <div className={"container_description"}>
+            <Description/>
+        </div>
+        <div className={"fab"}>
+            <a href="#canvas">
+                <Fab color="secondary" aria-label="edit">
+                    <PlayArrowIcon/>
+                </Fab>
+            </a>
+        </div>
+        <div id={"#canvas"}>
+            sss
+        </div>
+        <footer className={"footer"}>
+            &copy; Sujenan Thirumamany & Ismail Solombrino
+        </footer>
     </div>
   );
 }
---
+++ voronoifrontend/src/components/Description.js
@@ -0,0 +1,16 @@
+export default function Description(props){
+    return(
+        <>
+            <div className={"description"}>
+                <div className={"paragraph_description"}>
+                    <h1>Was ist Voronoi</h1><p>
+                    Im Rahmen des Unterrichts der angewandten Mathematik hatten wir die Möglichkeit das mathematische Vornoi-Diagramm, auch Thiessen-Polygone oder Dirichlet-Zerlegung genannt, in einer Applikation umzuwandeln.
+                </p>
+                    <p>
+                        Das Projekt wurde von zwei IMS-Schüler, <b>Sujenan Thirumamany und Ismail Solombrino</b> realisiert
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
